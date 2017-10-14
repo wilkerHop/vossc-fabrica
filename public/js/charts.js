936f7d255be2771b035c35e78bdf630f3c4b5a59
Wilker Ribeiro <rekliw97@gmail.com>
2017-10-14 16:08:46 -0300

    alterações

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,33 +1,56 @@
 $.getJSON('../DATA/test.json', function (data) {
 
-    Highcharts.chart('forn1', {
+	Highcharts.chart('forn1', {
 
-        title: {
-            text: 'Grafico 1'
-        },
+		title: {text: 'Grafico 1'},
 
-        xAxis: {
-            tickInterval: 7
-        },
+		xAxis: {tickInterval: 7},
 
-        yAxis: {
-            type: 'logarithmic',
-            tickInterval: 1,
-            minorTickInterval: 0.1
-        },
+		yAxis: {
+			type: 'logarithmic',
+			tickInterval: 1,
+			minorTickInterval: 0.1
+		},
 
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br />',
-            pointFormat: 'Dias da Semana = {point.x}, Ocorrencias = {point.y}'
-        },
+		tooltip: {
+			headerFormat: '<b>{series.name}</b><br />',
+			pointFormat: 'Dias da Semana = {point.x}, Ocorrencias = {point.y}'
+		},
 
-        series: [{
-            data: data.fornecedor1,
-            pointStart: 1,
-            label: {
-                enabled: false
-            }
-        }]
-    });
-    
+		series: [{
+			data: data.fornecedor1,
+			pointStart: 1,
+			label: {enabled: false}
+		}]
+	});
+	Highcharts.chart('forn2', {
+
+		title: {
+			text: 'Grafico 1'
+		},
+
+		xAxis: {
+			tickInterval: 7
+		},
+
+		yAxis: {
+			type: 'logarithmic',
+			tickInterval: 1,
+			minorTickInterval: 0.1
+		},
+
+		tooltip: {
+			headerFormat: '<b>{series.name}</b><br />',
+			pointFormat: 'Dias da Semana = {point.x}, Ocorrencias = {point.y}'
+		},
+
+		series: [{
+			data: data.fornecedor1,
+			pointStart: 1,
+			label: {
+				enabled: false
+			}
+		}]
+	});
+
 });
