9344c6a791ab165524360a642a791f31ebcc1b4f
Wilker Ribeiro <rekliw97@gmail.com>
2017-10-15 14:36:33 -0200

    alterações...

--- git.sh
+++ git.sh
@@ -1,1 +1,6 @@
-git add . && git commit -m'alterações' && git push origin master
+if test $0 -n
+then
+git add . && git commit -m'$0' && git push origin master
+else
+git add . && git commit -m'alterações...' && git push origin master
+fi
