01aea1f44fb4063b7792785cc82f43289c658513
Wilker Ribeiro <rekliw97@gmail.com>
2017-10-15 14:43:26 -0200

    $0

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 if test $0 -n
 then
-git add . && git commit -m'$0' && git push origin master
-else
 git add . && git commit -m'alterações...' && git push origin master
+else
+git add . && git commit -m'$0' && git push origin master
 fi
