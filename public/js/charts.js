9bc122f36158921b1dbb1b0affb16cfc15026696
Wilker Ribeiro <rekliw97@gmail.com>
2017-10-15 14:29:03 -0200

    alterações

--- public/js/charts.js
+++ public/js/charts.js
@@ -29,36 +29,62 @@ $.getJSON('../DATA/test.json', function (data) {
 			}
 		}]
 	});
+});
+
+$.getJSON('../DATA/forn2.json', function (data) {
+console.log(data)
 	Highcharts.chart('forn2', {
-
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false,
+			type: 'pie'
+		},
 		title: {
-			text: 'Grafico 1'
+			text: 'Browser market shares January, 2015 to May, 2015'
 		},
-
-		xAxis: {
-			tickInterval: 7
-		},
-
-		yAxis: {
-			type: 'logarithmic',
-			tickInterval: 1,
-			minorTickInterval: 0.1
-		},
-
 		tooltip: {
-			headerFormat: '<b>{series.name}</b><br />',
-			pointFormat: 'Dias da Semana = {point.x}, Ocorrencias = {point.y}'
+			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
 		},
-
-		series: [{
-			data: data,
-			pointStart: 1,
-			label: {
-				enabled: false
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					}
+				}
 			}
+		},
+		series: [{
+			name: 'Brands',
+			colorByPoint: true,
+			data: [{
+				name: 'Microsoft Internet Explorer',
+				y: 56.33
+			}, {
+				name: 'Chrome',
+				y: 24.03,
+				sliced: true,
+				selected: true
+			}, {
+				name: 'Firefox',
+				y: 10.38
+			}, {
+				name: 'Safari',
+				y: 4.77
+			}, {
+				name: 'Opera',
+				y: 0.91
+			}, {
+				name: 'Proprietary or Undetectable',
+				y: 0.2
+			}]
 		}]
 	});
-
 });
 
 $.getJSON('../DATA/stock.json', function (data) {
